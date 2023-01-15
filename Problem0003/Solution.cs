a6468624d730ce8dc39ce869135693c1a6dfd544
Rob Anderson <robander@outlook.com>
2023-01-15 13:38:56 -0600

    Solution for leetcode problem 3.

---
+++ Problem0003/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace Problem0003;
+
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        int result = 0;
+        Dictionary<char, int> charDict = new Dictionary<char, int>();
+
+        int start = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            char c = s[i];
+            if (charDict.ContainsKey(c))
+            {
+                start = Math.Max(charDict[c]+1, start);
+            }
+            result = Math.Max(result, i - start + 1);
+            charDict[c] = i;
+        }
+
+        return result;
+    }
+}
